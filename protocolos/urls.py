e8db6417cc7552e0dd3eafbcbaf91f52b18fe862
Professor <rafael.zottesso@ifpr.edu.br>
2025-04-24 21:08:38 -0300

    Criação de views para Update e Delete

--- protocolos/urls.py
+++ protocolos/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
 
 # Importar suas views
-from .views import (
-    CampusCreate,
-    CursoCreate,
-    StatusCreate,
-    TipoSolicitacaoCreate,
-    ServidorCreate,
-    AlunoCreate,
-    SolicitacaoCreate,
-    HistoricoCreate,
-)
+from .views import CampusCreate,CursoCreate,StatusCreate,TipoSolicitacaoCreate,ServidorCreate,AlunoCreate,SolicitacaoCreate,HistoricoCreate
+from .views import CampusUpdate, CursoUpdate, StatusUpdate, TipoSolicitacaoUpdate, ServidorUpdate, AlunoUpdate, SolicitacaoUpdate, HistoricoUpdate
+from .views import CampusDelete, CursoDelete, StatusDelete, TipoSolicitacaoDelete, ServidorDelete, AlunoDelete, SolicitacaoDelete, HistoricoDelete
 
 urlpatterns = [
     path("cadastrar/campus/", CampusCreate.as_view(), name="cadastrar-campus"),
@@ -21,4 +14,23 @@ urlpatterns = [
     path("cadastrar/aluno/", AlunoCreate.as_view(), name="cadastrar-aluno"),
     path("cadastrar/solicitacao/", SolicitacaoCreate.as_view(), name="cadastrar-solicitacao"),
     path("cadastrar/historico/", HistoricoCreate.as_view(), name="cadastrar-historico"),
+
+    path("editar/campus/<int:pk>/", CampusUpdate.as_view(), name="editar-campus"),
+    path("editar/curso/<int:pk>/", CursoUpdate.as_view(), name="editar-curso"),
+    path("editar/status/<int:pk>/", StatusUpdate.as_view(), name="editar-status"),
+    path("editar/tipo-solicitacao/<int:pk>/", TipoSolicitacaoUpdate.as_view(), name="editar-tipo-solicitacao"),
+    path("editar/servidor/<int:pk>/", ServidorUpdate.as_view(), name="editar-servidor"),
+    path("editar/aluno/<int:pk>/", AlunoUpdate.as_view(), name="editar-aluno"),
+    path("editar/solicitacao/<int:pk>/", SolicitacaoUpdate.as_view(), name="editar-solicitacao"),
+    # path("editar/historico/<int:pk>/", HistoricoUpdate.as_view(), name="editar-historico"),
+
+    path("excluir/campus/<int:pk>/", CampusDelete.as_view(), name="excluir-campus"),
+    path("excluir/curso/<int:pk>/", CursoDelete.as_view(), name="excluir-curso"),
+    path("excluir/status/<int:pk>/", StatusDelete.as_view(), name="excluir-status"),
+    path("excluir/tipo-solicitacao/<int:pk>/", TipoSolicitacaoDelete.as_view(), name="excluir-tipo-solicitacao"),
+    path("excluir/servidor/<int:pk>/", ServidorDelete.as_view(), name="excluir-servidor"),
+    path("excluir/aluno/<int:pk>/", AlunoDelete.as_view(), name="excluir-aluno"),
+    path("excluir/solicitacao/<int:pk>/", SolicitacaoDelete.as_view(), name="excluir-solicitacao"),
+    path("excluir/historico/<int:pk>/", HistoricoDelete.as_view(), name="excluir-historico"),
+
 ]
--- protocolos/views.py
+++ protocolos/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
@@ -89,3 +89,170 @@ class HistoricoCreate(CreateView):
     extra_context = {
         "titulo": "Cadastro de Histórico"
     }
+
+
+###############################################################
+
+
+class CampusUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Campus
+    success_url = reverse_lazy("index")
+    fields = ["nome"]
+    extra_context = {
+        "titulo" : "Atualizar dados do Campus"
+    } 
+
+
+class CursoUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Curso
+    success_url = reverse_lazy("index")
+    fields = ["nome", "campus"]
+    extra_context = {
+        "titulo" : "Atualização de Curso"
+    }
+
+
+class StatusUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Status
+    success_url = reverse_lazy("index")
+    fields = ["nome", "ordem", "pode_editar"]
+    extra_context = {
+        "titulo" : "Atualização de Status"
+    }
+
+
+class TipoSolicitacaoUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = TipoSolicitacao
+    success_url = reverse_lazy("index")
+    fields = [
+        "descricao",
+        "prazo_externo",
+        "prazo_externo_dias",
+        "prazo_interno",
+        "prazo_interno_dias",
+    ]
+    extra_context = {
+        "titulo": "Atualização de Tipo de Solicitação"
+    }
+
+
+class ServidorUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Servidor
+    success_url = reverse_lazy("index")
+    fields = ["nome", "siape", "email"]
+    extra_context = {
+        "titulo": "Atualização de Servidor"
+    }
+
+
+class AlunoUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Aluno
+    success_url = reverse_lazy("index")
+    fields = ["nome", "matricula", "cpf", "email", "telefone"]
+    extra_context = {
+        "titulo": "Atualização de Aluno"
+    }
+
+
+class SolicitacaoUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Solicitacao
+    success_url = reverse_lazy("index")
+    fields = ["solicitado_por", "curso", "turma", "tipo_solicitacao", "justificativa", "anexo"]
+    extra_context = {
+        "titulo": "Atualização de Solicitação"
+    }
+
+
+class HistoricoUpdate(UpdateView):
+    template_name = "protocolos/form.html"
+    model = Historico
+    success_url = reverse_lazy("index")
+    fields = ["solicitacao", "status", "gerado_por"]
+    extra_context = {
+        "titulo": "Atualização de Histórico"
+    }
+
+
+###############################################################
+
+
+class CampusDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Campus
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Campus",
+    }
+
+
+class CursoDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Curso
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Curso",
+    }
+
+
+class StatusDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Status
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Status",
+    }
+
+
+class TipoSolicitacaoDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = TipoSolicitacao
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Tipo de Solicitação",
+    }
+
+
+class ServidorDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Servidor
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Servidor",
+    }
+
+
+class AlunoDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Aluno
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Aluno",
+    }
+
+
+class SolicitacaoDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Solicitacao
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Solicitação",
+    }
+
+
+class HistoricoDelete(DeleteView):
+    template_name = "protocolos/form-excluir.html"
+    model = Historico
+    success_url = reverse_lazy("index")
+    extra_context = {
+        "titulo" : "Excluir Histórico",
+    }
+
+
+
